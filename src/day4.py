6dba3bfd1f7ff8b2c3d4a07f3bfa629f712f9ab8
Ben Jones <fetiddius@gmail.com>
2024-12-13 09:16:05 +0000

    day4a done!

--- src/day4.py
+++ src/day4.py
@@ -8,36 +8,38 @@ class Day4:
 		return self.find(data, line_length=line_length)
 
 	def find(self, grid, line_length=10):
-		print(grid)
+		# print(grid)
 		count = 0
 		for i in range(len(grid)):
 			if grid[i] == 'X':
 				# try to find XMAS
+				# print(f"searching from {i}...")
 				multipliers = (
-					line_length - 1,
 					line_length,
 					line_length + 1,
+					line_length + 2,
 					1,
 				)
 				for multiplier in multipliers:
 					for mod in (lambda i, f: i - f, lambda i, f: i + f):
 						found = True
-						print(f"searching from {i} for:")
 						for x in range(3):
 							search_index = mod(i, ((x + 1) * multiplier))
-							print(f"\t{TARGET[x]} at {search_index}", end=" ")
+							# print(f"\t@{search_index}", end="")
 							if 0 <= search_index < len(grid):
 								if grid[search_index] != TARGET[x]:
-									print("but couldn't find it!")
+									# print("but couldn't find it!")
+									# print("")
 									found = False
 									break
-								print()
+								# print(f" and found {TARGET[x]}")
 							else:
-								print("but went off the end of the grid!")
+								# print("but went off the end of the grid!")
+								# print("")
 								found = False
 								break
 						if found:
-							print("\tfound XMAS!")
+							# print("\tfound XMAS!")
 							count += 1
 		return count
 
