2661c1ce7aafbe4577f9bfd73f514f2b49ab1424
Ben Jones <fetiddius@gmail.com>
2024-12-24 08:39:05 +0000

    segfault???

--- rs/src/day10.rs
+++ rs/src/day10.rs
@@ -9,10 +9,13 @@ fn main() {
     println!("{}", result);
 }
 
+type RelMap<'a> = HashMap<(usize, usize), (&'a Tile, HashSet<(usize, usize)>)>;
+
 fn process(input: &str) -> String {
-    let mut grid: Grid<Tile> = Grid::new(input);
+    let grid: Grid<Tile> = Grid::new(input);
     let mut scores = 0;
-    let mut rels: HashMap<(usize, usize), (&Tile, HashSet<(usize, usize)>)> = HashMap::new();
+    let mut rels: RelMap = HashMap::new();
+    println!("processing grid...");
     for (y, inner) in grid.tiles.iter().enumerate() {
         for (x, tile) in inner.iter().enumerate() {
             rels.insert((x, y), (&tile, HashSet::new()));
@@ -50,8 +53,35 @@ fn process(input: &str) -> String {
             }
         }
     }
-    for (x, y) in rels.keys() {}
-    todo!();
+    println!("calculating...");
+    println!("{:?}", rels.keys());
+    println!("{:?}", rels.values());
+    for (tile, neighbours) in rels.values() {
+        if tile.height() == 0 {
+            for n in neighbours {
+                let sc = calculate_score(&n, &rels);
+                scores += sc;
+            }
+        }
+    }
+    scores.to_string()
+}
+
+fn calculate_score(coord: &(usize, usize), rels: &RelMap) -> u32 {
+    if let Some((tile, neighbours)) = rels.get(&coord) {
+        if tile.height() == 9 {
+            1
+        } else {
+            let mut score = 0;
+            for n in neighbours {
+                let sc = calculate_score(n, rels);
+                score += sc;
+            }
+            score
+        }
+    } else {
+        0
+    }
 }
 
 impl Grid<Tile> {}
@@ -97,8 +127,41 @@ mod tests {
 						   01329801\n\
 						   10456732";
 
+    // #[ignore]
     #[test]
     fn test_process() {
         assert_eq!("36".to_string(), process(EXAMPLE));
+    }
+
+    #[test]
+    fn test_calculate_score() {
+        let tile_0 = Tile::Trailhead;
+        let tile_1 = Tile::Level(1);
+        let tile_2 = Tile::Level(2);
+        let tile_3 = Tile::Level(3);
+        let tile_4 = Tile::Level(4);
+        let tile_5 = Tile::Level(5);
+        let tile_6 = Tile::Level(6);
+        let tile_7 = Tile::Level(7);
+        let tile_8 = Tile::Level(8);
+        let tile_9 = Tile::Peak;
+        let rels = HashMap::from([
+            ((1, 1), (&tile_0, HashSet::from([(1, 2)]))),
+            ((1, 2), (&tile_1, HashSet::from([(1, 3)]))),
+            ((1, 3), (&tile_2, HashSet::from([(1, 4)]))),
+            ((1, 4), (&tile_3, HashSet::from([(1, 5)]))),
+            ((1, 5), (&tile_4, HashSet::from([(1, 6)]))),
+            ((1, 6), (&tile_5, HashSet::from([(1, 7)]))),
+            ((1, 7), (&tile_6, HashSet::from([(1, 8)]))),
+            ((1, 8), (&tile_7, HashSet::from([(1, 9)]))),
+            ((1, 9), (&tile_8, HashSet::from([(1, 10)]))),
+            ((1, 10), (&tile_9, HashSet::new())),
+        ]);
+        assert_eq!(1, calculate_score(&(1, 1), &rels));
+        for (tile, neighbours) in rels.values() {
+            for n in neighbours {
+                println!("{}", rels.get(&n).unwrap().0.height());
+            }
+        }
     }
 }
