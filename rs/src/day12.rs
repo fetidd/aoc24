e79dc821caeb6f676a2dd6c84d58883ecf81f49d
Ben Jones <fetiddius@gmail.com>
2025-01-08 12:17:03 +0000

    derp

--- rs/src/day12.rs
+++ rs/src/day12.rs
@@ -1,20 +1,56 @@
-use aoc24::utils;
+use std::collections::HashMap;
+
+use aoc24::utils::{
+    self,
+    grid::{Grid, GridCursor, GridPos},
+};
 
 fn main() {
-	let input = include_str!("../../puzzle_input/day12.txt");
-	let result = process(&input);
-	println!("{}", result);
+    let input = include_str!("../../puzzle_input/day12.txt");
+    let result = process(&input);
+    println!("{}", result);
 }
 
 fn process(input: &str) -> String {
-	input.to_string()
+    let grid: Grid<char> = Grid::new(input);
+    let mut region_ids: HashMap<char, usize> = HashMap::new();
+    let mut regions: Vec<Region> = vec![];
+    for (pos, char) in &grid {
+        let mut cursor = grid.cursor();
+        cursor.goto(pos);
+        let mut region = Region::new(*char, &mut region_ids).build(cursor);
+    }
+    todo!()
+}
+
+struct Region {
+    id: usize,
+    kind: char,
+    content: Option<Vec<GridPos>>,
+}
+
+impl Region {
+    fn new(kind: char, ids: &mut HashMap<char, usize>) -> Self {
+        ids.entry(kind).and_modify(|e| *e += 1).or_insert(0);
+        Self {
+            kind,
+            id: ids[&kind],
+            content: None,
+        }
+    }
+
+    fn build(self, cursor: GridCursor<char>) -> Self {
+        let content = vec![];
+        let content = Some(content);
+        Self { content, ..self }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	const EXAMPLE: &str = "RRRRIICCFF\n\
+    const EXAMPLE: &str = "RRRRIICCFF\n\
 						   RRRRIICCCF\n\
 						   VVRRRCCFFF\n\
 						   VVRCCCJFFF\n\
@@ -25,22 +61,19 @@ mod tests {
 						   MIIISIJEEE\n\
 						   MMMISSJEEE";
 
-	#[test]
-	fn test_process() {
-		assert_eq!("1930".to_string(), process(EXAMPLE));
-	}
+    #[test]
+    fn test_process() {
+        assert_eq!("1930".to_string(), process(EXAMPLE));
+    }
 
-	#[test]
-	fn test_calculate_region() {
-		let tests = [
-			("RRRR.\n\
-			  RRRR.\n\
-			  ..RRR\n\
-			  ..R..", 216),
-		];
-		for (region, expected) in tests {
-			assert_eq!(expected, calculate_region(region));
-		}
-	}
+    // #[test]
+    // fn test_calculate_region() {
+    //     let tests = [
+    //         ("RRRR.\nRRRR.\n..RRR\n..R..", 216),
+    //         ("....\n.II.\n.II.\n....", 32),
+    //     ];
+    //     for (region, expected) in tests {
+    //         assert_eq!(expected, calculate_region(region));
+    //     }
+    // }
 }
-
