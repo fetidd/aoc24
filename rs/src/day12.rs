54567c766d86d8e964a0d0239583e3eced810f84
Ben Jones <fetiddius@gmail.com>
2025-01-08 07:29:48 +0000

    derp

--- rs/src/day12.rs
+++ rs/src/day12.rs
@@ -14,10 +14,33 @@ fn process(input: &str) -> String {
 mod tests {
 	use super::*;
 
+	const EXAMPLE: &str = "RRRRIICCFF\n\
+						   RRRRIICCCF\n\
+						   VVRRRCCFFF\n\
+						   VVRCCCJFFF\n\
+						   VVVVCJJCFE\n\
+						   VVIVCCJJEE\n\
+						   VVIIICJJEE\n\
+						   MIIIIIJJEE\n\
+						   MIIISIJEEE\n\
+						   MMMISSJEEE";
+
 	#[test]
 	fn test_process() {
-		assert_eq!("input".to_string(), process("input"));
+		assert_eq!("1930".to_string(), process(EXAMPLE));
+	}
+
+	#[test]
+	fn test_calculate_region() {
+		let tests = [
+			("RRRR.\n\
+			  RRRR.\n\
+			  ..RRR\n\
+			  ..R..", 216),
+		];
+		for (region, expected) in tests {
+			assert_eq!(expected, calculate_region(region));
+		}
 	}
 }
-
 
